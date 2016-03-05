24a7ecd35badd5aed6fc5c4c3025faa124fac76e
Graham Zuber <grahamzuber@gmail.com>
2016-03-04 20:20:32 -0700

    Initial Commit

---
+++ wordsmash/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  wordsmash
+//
+//  Created by Graham Zuber on 3/4/16.
+//  Copyright © 2016 zuber.io. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
